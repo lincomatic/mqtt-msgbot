1c98ff8444870740e2768d6b0ba3879daaf5d0ef
lincomatic <lincomatic@yahoo.com>
2025-12-24 15:15:14 -0800

    multi region start

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 source bin/activate
-nohup python mqtt_msgbot.py > output.txt 2>&1 &
+nohup python mqtt_msgbot.py -c config-socal.ini </dev/null > socal.txt 2>&1 &
+nohup python mqtt_msgbot.py -c config-sfbay.ini </dev/null > sfbay.txt 2>&1 &
+nohup python mqtt_msgbot.py -c config-sac.ini </dev/null > sac.txt 2>&1 &
+ps ax | grep msgbot
